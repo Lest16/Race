74930f13b1f99b699de46a0cbafbbc8b0ddd4776
Lest <piligrim16@mail.ru>
2017-07-05 20:42:21 +0300

    infinity move road

---
+++ Race/Assets/Scripts/MoveRoad.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class MoveRoad : MonoBehaviour
+    {
+        public float Speed = 0.9f;
+
+        private Vector3 startPosition;
+
+        public void Start () {
+            startPosition = transform.position;
+        }
+
+        public void Update () {
+            Debug.Log(transform.position.y);
+            var newPosition = Mathf.Repeat(Time.time * Speed, 7.17f);
+            transform.position = startPosition + Vector3.up * newPosition;
+        }
+    }
+}
